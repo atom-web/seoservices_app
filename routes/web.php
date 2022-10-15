d51e052130048b6f585186d5032ee82cd32decf7
sevenee <olologvinov.o@gmail.com>
2022-10-15 15:02:24 +0300

    homepage

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd($_SERVER);
-    // return view('welcome');
+    // dd($_SERVER);
+    return view('welcome');
 });
